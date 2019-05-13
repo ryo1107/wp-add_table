c1b97e302e0c66b219f966833486a4d445d36926
ryo.k <41412168+ryo1107@users.noreply.github.com>
2019-05-13 17:13:17 +0900

    change add_action & function

--- wp-add_table.php
+++ wp-add_table.php
@@ -8,17 +8,15 @@ Version: 0.0.0
 Author URL: https://github.com/ryo1107
 */
 
-function after_title_menu(){?>
-    <script>
-    function add_table(){
-    var $origin_data = $('#content');
-    var $table_data = '\n\n| Col 1 | Col 2 | Col 3 |\n| -------- | -------- | -------- |\n| Text | Text | Text |\n\n';
-    $origin_data.val($origin_data.val()+$table_data);
-    }
-    </script>
-    <input type="button" value="add table" onClick="add_table()"/>
-<?php }
-//add_action( 'edit_form_after_editor', after_editor_menu );
-add_action( 'edit_form_after_title', after_title_menu );
+function themes_add_quicktags () {
+	if ( wp_script_is( 'quicktags' ) ) {
+		$html  = '<script>';
+		$html .= 'QTags.addButton( "eg_paragraph", "table", "\n\n| Col 1 | Col 2 | Col 3 |\n| -------- | -------- | -------- |\n| Text | Text | Text |\n\n", "", "table", "Paragraph tag", 1 );';
+		$html .= '</script>';
+
+		echo $html;
+	}
+}
+add_action( 'admin_print_footer_scripts', 'themes_add_quicktags' );
 
 ?>'}
